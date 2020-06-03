e47905965466e35e490e731d9e01c5efabec681e
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-06-03 23:11:01 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,12 +2,13 @@
 class Person 
   @@all = [] 
   
-  attr_accessor :name 
+  attr_accessor :name, :bank, :happiness, :hygiene
   
   def initialize
   @name = name 
   @bank = 25 
   @happiness = 8 
+  @hygiene = 8 
   end 
 
 end 
