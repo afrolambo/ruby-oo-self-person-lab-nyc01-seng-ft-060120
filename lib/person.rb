78ae2987edb6a79bfd63f09271eda9c105c53268
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-06-04 00:39:42 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -55,13 +55,13 @@ class Person
   end 
   
   def start_conversation(person, topic)
-    object = [self, person] 
-  if topic == "politics"
-    object.each {|o| o.happiness -= 2}
-    first, second = ["partison", "lobbyist"]
-  else topic == "weather"
-    object.each {|o| o.happiness += 1} 
-    first, second = ["sun","rain"]
+      objects = [self, person]
+    if topic == "politics"
+      objects.each { |o| o.happiness -= 2}
+      first, second = ["partisan", "lobbyist"]
+    elsif topic == "weather"
+      objects.each { |o| o.happiness += 1}
+      first, second = ["sun", "rain"]
     end 
     first ||= "blah"
     second ||= "blah"
